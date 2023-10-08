acf773c7dd3a21e8520065eb8af90e5fcb455551
HumanWeapon <odair.umanzor@unah.hn>
2023-10-08 12:51:11 -0600

    carga de la tabla usuarios

--- src/app/pages/mantenimiento/usuarios/usuarios.component.ts
+++ src/app/pages/mantenimiento/usuarios/usuarios.component.ts
@@ -10,19 +10,68 @@ import { UsuariosService } from 'src/app/services/usuarios.service';
 export class UsuariosComponent implements OnInit{
 
   listUsuarios: Usuario[] = [];
+  constructor(private _userService: UsuariosService) { }
 
-  constructor(private _userService: UsuariosService){}
-
+  dtOptions: DataTables.Settings = {};
   
   ngOnInit(): void {
-    this.getAllUsers();
+    this.dtOptions = {
+      ajax: (dataTablesParameters: any, callback) => {
+        this._userService.getAllUsuarios().subscribe((usuarios) => {
+          // Mapea los datos de usuarios en el formato esperado por DataTables
+          const dataTableData = {
+            recordsTotal: usuarios.length,
+            recordsFiltered: usuarios.length,
+            data: usuarios,
+          };
+          callback(dataTableData);
+        });
+      },
+      columns: [
+        {
+          title: 'Usuario',
+          data: 'usuario',
+        },
+        {
+          title: 'Nombre',
+          data: 'nombre_usuario',
+        },
+        {
+          title: 'Correo',
+          data: 'correo_electronico',
+        },
+        {
+          title: 'Estado',
+          data: 'estado_usuario',
+        },
+        {
+          title: 'Id Rol',
+          data: 'id_rol',
+        },
+        {
+          title: 'Última conexión',
+          data: 'fecha_ultima_conexion',
+        },
+        {
+          title: 'Creado por',
+          data: 'creado_por',
+        },
+        {
+          title: 'Fecha creación',
+          data: 'fecha_creacion',
+        },
+        {
+          title: 'Vencimiento',
+          data: 'fecha_vencimiento',
+        }
+      ],
+    };
   }
 
-
-  getAllUsers(){
+  getAllUsers() {
     this._userService.getAllUsuarios().subscribe(data => {
       this.listUsuarios = data;
-      console.log(this.listUsuarios)
-    })
+      console.log(this.listUsuarios);
+    });
   }
 }
