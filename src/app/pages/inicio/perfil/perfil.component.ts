c27693d9b569f193ae73ccb1432fa1abc3b16d8f
imsamv <ismael.midence@unah.hn>
2023-10-16 00:13:34 -0600

    Cambios en diseño y algunas validaciones

--- src/app/pages/inicio/perfil/perfil.component.ts
+++ src/app/pages/inicio/perfil/perfil.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { UsuariosService } from '../../../services/usuarios.service';
-import { Usuario } from 'src/app/interfaces/usuario';
-import { FormGroup,FormControl,Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-perfil',
@@ -10,68 +8,38 @@ import { FormGroup,FormControl,Validators } from '@angular/forms';
 })
 
 export class PerfilComponent  implements OnInit{
-  
-    
-  constructor (private _userService: UsuariosService) {}
+oldpassword:string='';
+newpassword:string='';
+confirmpassword:string='';
 
-    datosUsuario!:Usuario;
-     editarform = new FormGroup({
-         usuario: new FormControl(''),
-         nombre: new FormControl(''),
-         email: new FormControl(''),
-     });
+  inputDeshabilitado: boolean = true; // input Deshabilitado/bloqueado
      
+  constructor( private toastr: ToastrService) {
 
+
+  }
    
   ngOnInit(): void {
-
- /*this.getUsuario();*/
-
- this._userService.getAllUsuarios().subscribe(data => {
-  this.datosUsuario = data [8];
-  this.editarform.setValue({
-   'usuario': this.datosUsuario.usuario,
-   'nombre': this.datosUsuario.nombre_usuario,
-   'email': this.datosUsuario.correo_electronico,
- });
-
- console.log(this.editarform.value);
-});
-
-  }
-
-    /*getUsuario(){
-      const user: Usuario = {
-        usuario: this.usuarioLogin,
-        contrasena: '',
-        id_usuario: 0,
-        creado_por: '',
-        fecha_creacion: new Date(),
-        modificado_por: '',
-        fecha_modificacion: new Date(),
-        nombre_usuario: '',
-        correo_electronico: '',
-        estado_usuario: false,
-        id_rol: 0,
-        fecha_ultima_conexion: new Date(),
-        primer_ingreso: new Date(),
-        fecha_vencimiento: new Date(),
-        intentos_fallidos: 0
-      }
-
-      this._userService.getUsuario(user).subscribe(data => {
-        this.usuario = data;
-       this.editarform.setValue({
-         'usuario': this.datosUsuario.usuario,
-         'nombre': this.datosUsuario.nombre_usuario,
-         'email': this.datosUsuario.correo_electronico,
-       });
-       console.log(this.editarform.value);
-      });
- 
-    }*/
-
   }
   
+  habilitarInput() {
+    this.inputDeshabilitado = false;
+  }
+
+  deshabilitarInput() {
+    this.inputDeshabilitado = true;
+  }
+
+  passwordChanged() {
+    if (this.oldpassword == '' || this.newpassword == '') {
+      this.toastr.error('Todos los campos son obligatorios', 'Error');
+      return 
+    }
+}
+
+
+
+
+}
 
 
