392b7aad55fae571cd0f3f85f76bd930c8509e2c
HumanWeapon <odair.umanzor@unah.hn>
2023-10-28 20:47:23 -0600

    Save local changes to usuarios.component.ts

--- src/app/pages/mantenimiento/usuarios/usuarios.component.ts
+++ src/app/pages/mantenimiento/usuarios/usuarios.component.ts
@@ -77,7 +77,7 @@ export class UsuariosComponent implements OnInit  {
   ngOnInit(): void {
     this.dtOptions = {
       pagingType: 'full_numbers',
-      pageLength: 8,
+      pageLength: 15,
       language: { url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json' },
       responsive: true,
     };
